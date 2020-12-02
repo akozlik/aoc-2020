7deed3b1b26828a0cc4bd5f177f23b3622c13724
akozlik <akozlik@gmail.com>
2020-12-02 12:38:04 -0500

    Added regex parsing

--- day2/day2.js
+++ day2/day2.js
@@ -2,7 +2,8 @@ const input = require("./input.json");
 
 var count = 0;
 input.passwords.forEach((v, i) => {
-    var input = parseInput(v);
+    var input = parseInputWithRegex(v);
+    
     var regex = new RegExp("[^" + input.character + "]+", "g");
     var newPassword = input.password.replace(regex, "");
     if (newPassword.length >= input.min && newPassword.length <= input.max) {
@@ -12,7 +13,7 @@ input.passwords.forEach((v, i) => {
 console.log("Part 1 Looped: " + count);
 
 var temp = input.passwords.filter((v, i) => {
-    var input = parseInput(v);
+    var input = parseInputWithRegex(v);
     var regex = new RegExp("[^" + input.character + "]+", "g");
     var newPassword = input.password.replace(regex, "");
     return (newPassword.length >= input.min && newPassword.length <= input.max) 
@@ -22,7 +23,7 @@ console.log("Part 1 Filtered: " + temp.length);
 
 count = 0;
 var part2 = input.passwords.filter((v, i) => {
-    var input = parseInput(v);
+    var input = parseInputWithRegex(v);
     var first = input.password[input.min-1];
     var last = input.password[input.max-1];
     return (first != last && (first === input.character || last === input.character))
@@ -30,7 +31,20 @@ var part2 = input.passwords.filter((v, i) => {
 
 console.log("Part 2: " + part2.length);
 
-function parseInput(input) {
+function parseInputWithRegex(input) {
+    var parts = input.match(/(\d+)-(\d+) (\w): (\w+)/);
+
+    var result = {
+        min: parts[1],
+        max: parts[2],
+        character: parts[3],
+        password: parts[4]
+    }
+
+    return result;
+}
+
+function parseInputWithSplit(input) {
     var temp = input.split(" ");
     var range = temp[0].split("-");
     const min = range[0];
